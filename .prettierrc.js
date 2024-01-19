404670a04bd54545b8b52538f73852c81092d1da
Amgad Hussein <amgadhussain.iqranetwork@gmail.com>
2024-01-19 13:38:07 +0200

    setup project default settings & code formatter

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 150,
+  semi: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  singleQuote: true,
+  proseWrap: 'always',
+  quoteProps: 'consistent',
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  requireConfig: true,
+};
