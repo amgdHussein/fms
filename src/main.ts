15d7f4d7d3e6f10c5a23ddd54e58f32f28a2d0ae
Amgad Hussein <amgadhussain.iqranetwork@gmail.com>
2024-01-19 13:54:48 +0200

    setup project/swagger configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Create the Nest application
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Set up basic authentication for the API endpoints [Swagger]
+  app.use(
+    [process.env.SWAGGER_PATH || 'api'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER || 'admin']: process.env.SWAGGER_PASSWORD || '0000',
+      },
+    }),
+  );
+
+  // Enable Cross-Origin Resource Sharing (CORS)
+  app.enableCors();
+
+  // Create the Swagger documentation configuration
+  const config = new DocumentBuilder()
+    .setTitle(process.env.APP_TITLE || 'FMS')
+    .setDescription(process.env.APP_DESCRIPTION || 'MFS backend endpoints.')
+    .setVersion(process.env.APP_VERSION || '0.0.1')
+    .build();
+
+  // Generate the Swagger document based on the application and configuration
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Set up the Swagger UI to serve the generated documentation
+  SwaggerModule.setup(process.env.SWAGGER_PATH || 'api', app, document);
+
+  // Start the application server
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
